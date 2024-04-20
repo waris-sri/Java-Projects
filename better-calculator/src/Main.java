d50604d61953f08403a172b01bc4e115e3e29c8d
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-04-21 03:32:57 +0700

    edited Main.java

--- better-calculator/src/Main.java
+++ better-calculator/src/Main.java
@@ -2,8 +2,8 @@
  * @author: Thanakrit S., Waris S.
  * @date: 20/4/2024
  * @description:
- * Calculates the result of a mathematical expression by converting
- * the input string into an array list of numbers and operations,
+ * The program calculates the result of a mathematical expression by
+ * converting the input string into an array list of numbers and operations,
  * such as "12 + 34 - 56 * 78 / 90", then performing the operations in
  * literal order from left to right.
  * 
