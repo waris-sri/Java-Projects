1b220c8bf549f9aff0a68e6e5fadbd913601cb15
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-04-23 23:50:34 +0700

    update code

--- ParenthesesPriority/src/Main.java
+++ ParenthesesPriority/src/Main.java
@@ -1,7 +1,9 @@
-
 public class Main {
-
 	public static void main(String[] args) {
-		System.out.println(Bracket.getOrderT("(Hello((Hi(WHy(This))Hi1)hi2))(second(innerS))(third(innerT))"));
+		System.out.println();
+		System.out.println(Bracket.getOrder("(Hello((Hi(WHy(This))Hi1)hi2)) (second(innerS)) (third(innerT))"));
+		System.out.println();
+		System.out.println(Bracket.getOrder("(12(34((56)(78(90)))) (AB(CD)) (EF))"));
+		System.out.println();
 	}
 }
