8593dcb68f4128974ee46f32d0dec3e4ee30f4c9
JAPAN <G480@G480-PC>
2024-04-23 17:47:47 +0700

    I Fucking did it lets gooooooooooooooooooooooooooooooooooooooooo!!!!!
    Refactor some code so it read the string properly

--- ParenthesesPriority/src/Main.java
+++ ParenthesesPriority/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[]args) {
-		System.out.println(Read.test("Hello(IDK(Hi(WHy)Hi1)hi2)",0));
+		System.out.println(Read.test("Hello((Hi(WHy)Hi1)hi2)",0));
 		
 	}
 }
