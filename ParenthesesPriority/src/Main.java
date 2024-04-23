97ed627439d1e6ba9e012fdb2455e4a7b77d94b7
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-04-23 23:19:28 +0700

    removed Scanner

--- ParenthesesPriority/src/Main.java
+++ ParenthesesPriority/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println(Bracket.getOrder("(Hello((Hi(WHy(This))Hi1)hi2))(second(innerS))(third(innerT))", 0));
