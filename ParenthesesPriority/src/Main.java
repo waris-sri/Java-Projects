e8f295d14165691d4b06388fcfd6ab9b303a64b9
G480 <G480@G480-PC>
2024-04-23 22:44:57 +0700

    Hello

--- ParenthesesPriority/src/Main.java
+++ ParenthesesPriority/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[]args) {
-		System.out.println(Read.test("Hello((Hi(WHy)Hi1)hi2)",0));
+		System.out.println(Read.test("Hello((HI(lol(LEtgo)OMG(UNbelievable))Hi)",0));
 		
 	}
 }
