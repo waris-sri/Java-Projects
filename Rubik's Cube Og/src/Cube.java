ef11f4e5e5d71a31ed15fada570054244f73682c
G480 <G480@G480-PC>
2024-05-28 20:36:57 +0700

    Added Rubik's Cube Project

---
+++ Rubik's Cube Og/src/Cube.java
@@ -0,0 +1,126 @@
+
+public class Cube {
+	String[][] front = new String[3][3]; 
+	String[][] back = new String[3][3];
+	String[][] left = new String[3][3];
+	String[][] right = new String[3][3];
+	String[][] up = new String[3][3];
+	String[][] down = new String[3][3];
+
+	Cube() {
+		insertAll("Y",up);
+		insertAll("W",down);
+		insertAll("R",front);
+		insertAll("G",right);
+		insertAll("B",left);
+		insertAll("O",back);
+	}
+	
+	private static String[][] insertAll(String str, String matrix[][]){
+		for(int i = 0 ;i < matrix.length ; i++) {
+			for(int j = 0; j < matrix[i].length;j++) {
+				matrix[i][j] = str;
+			}
+		}
+		return matrix;
+	}
+	
+	void left() {
+		String temp00 = up[0][0];
+		String temp10 = up[1][0];
+		String temp20 = up[2][0];
+		up[0][0] = back[0][0];
+		up[1][0] = back[1][0];
+		up[2][0] = back[2][0];
+		back[0][0] = down[0][0];
+		back[1][0] = down[2][0];
+		back[2][0] = down[2][0];
+		down[0][0] = front[0][0];
+		down[1][0] = front[1][0];
+		down[2][0] = front[2][0];
+		front[0][0] = temp00;
+		front[1][0] = temp10;
+		front[2][0] = temp20;
+		temp00 = left[0][0];
+		temp10 = left[1][0];
+		left[0][0] = left[2][0];
+		left[1][0] = left[2][1];
+		left[2][0] = left[2][2];
+		left[2][1] = left[1][2];
+		left[2][2] = left[0][2];
+		left[1][2] = left[0][1];
+		left[0][2] = temp00;
+		left[0][1] = temp10;
+	}
+	
+	void leftPrime() {
+		String temp00 = up[0][0];
+		String temp10 = up[1][0];
+		String temp20 = up[2][0];
+		up[0][0] = front[0][0];
+		up[1][0] = front[1][0];
+		up[2][0] = front[2][0];
+		front[0][0] = down[0][0];
+		front[1][0] = down[2][0];
+		front[2][0] = down[2][0];
+		down[0][0] = back[0][0];
+		down[1][0] = back[1][0];
+		down[2][0] = back[2][0];
+		back[0][0] = temp00;
+		back[1][0] = temp10;
+		back[2][0] = temp20;
+		temp00 = left[0][0];
+		temp10 = left[1][0];
+		left[0][0] = left[0][2];
+		left[1][0] = left[0][1];
+		left[0][2] = left[2][2];
+		left[0][1] = left[1][2];
+		left[2][2] = left[2][0];
+		left[1][2] = left[2][1];
+		left[2][0] = temp00;
+		left[2][1] = temp10;
+	}
+	
+	void print() {
+		for(String[] i : back) {
+			System.out.println("");
+			for(int k = 0 ; k < 6; k++) {
+				System.out.print(" ");
+			}
+			for(String j : i) {
+				System.out.print(" "+j);
+			}
+		}
+		for(int i = 0; i < 3 ; i++) {
+			System.out.println("");
+			for(String j : left[i]) {
+				System.out.print(" "+j);
+			}
+			for(String j : up[i]) {
+				System.out.print(" "+j);
+			}
+			for(String j : right[i]) {
+				System.out.print(" "+j);
+			}
+		}
+		for(String[] i : front) {
+			System.out.println("");
+			for(int k = 0 ; k < 6; k++) {
+				System.out.print(" ");
+			}
+			for(String j : i) {
+				System.out.print(" "+j);
+			}
+		}
+		for(String[] i : down) {
+			System.out.println("");
+			for(int k = 0 ; k < 6; k++) {
+				System.out.print(" ");
+			}
+			for(String j : i) {
+				System.out.print(" "+j);
+			}
+		}
+	}
+	
+}
---
+++ Rubik's Cube Og/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[]args){
+		Cube cube = new Cube();
+		cube.print();
+		cube.left();
+		System.out.println("");
+		cube.print();
+	}
+}
