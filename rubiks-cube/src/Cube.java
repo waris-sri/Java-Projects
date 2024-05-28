e18e7c62d9b3e00e628437b3ec614b3c009ec5b1
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-05-28 21:47:13 +0700

    Added m, mP, r, rP

--- rubiks-cube/src/Cube.java
+++ rubiks-cube/src/Cube.java
@@ -1,19 +1,19 @@
 
 public class Cube {
-	String[][] front = new String[3][3]; 
-	String[][] back = new String[3][3];
-	String[][] left = new String[3][3];
-	String[][] right = new String[3][3];
-	String[][] up = new String[3][3];
-	String[][] down = new String[3][3];
+	String[][] f = new String[3][3]; 
+	String[][] b = new String[3][3];
+	String[][] l = new String[3][3];
+	String[][] r = new String[3][3];
+	String[][] u = new String[3][3];
+	String[][] d = new String[3][3];
 
 	Cube() {
-		insertAll("Y",up);
-		insertAll("W",down);
-		insertAll("R",front);
-		insertAll("G",right);
-		insertAll("B",left);
-		insertAll("O",back);
+		insertAll("Y",u);
+		insertAll("W",d);
+		insertAll("R",f);
+		insertAll("G",r);
+		insertAll("B",l);
+		insertAll("O",b);
 	}
 	
 	private static String[][] insertAll(String str, String matrix[][]){
@@ -25,93 +25,213 @@ public class Cube {
 		return matrix;
 	}
 	
-	void left() {
-		String temp00 = up[0][0];
-		String temp10 = up[1][0];
-		String temp20 = up[2][0];
-		up[0][0] = back[0][0];
-		up[1][0] = back[1][0];
-		up[2][0] = back[2][0];
-		back[0][0] = down[0][0];
-		back[1][0] = down[2][0];
-		back[2][0] = down[2][0];
-		down[0][0] = front[0][0];
-		down[1][0] = front[1][0];
-		down[2][0] = front[2][0];
-		front[0][0] = temp00;
-		front[1][0] = temp10;
-		front[2][0] = temp20;
-		temp00 = left[0][0];
-		temp10 = left[1][0];
-		left[0][0] = left[2][0];
-		left[1][0] = left[2][1];
-		left[2][0] = left[2][2];
-		left[2][1] = left[1][2];
-		left[2][2] = left[0][2];
-		left[1][2] = left[0][1];
-		left[0][2] = temp00;
-		left[0][1] = temp10;
+	void l() {
+		String temp00 = u[0][0];
+		String temp10 = u[1][0];
+		String temp20 = u[2][0];
+		u[0][0] = b[0][0];
+		u[1][0] = b[1][0];
+		u[2][0] = b[2][0];
+		b[0][0] = d[0][0];
+		b[1][0] = d[2][0];
+		b[2][0] = d[2][0];
+		d[0][0] = f[0][0];
+		d[1][0] = f[1][0];
+		d[2][0] = f[2][0];
+		f[0][0] = temp00;
+		f[1][0] = temp10;
+		f[2][0] = temp20;
+		temp00 = l[0][0];
+		temp10 = l[1][0];
+		l[0][0] = l[2][0];
+		l[1][0] = l[2][1];
+		l[2][0] = l[2][2];
+		l[2][1] = l[1][2];
+		l[2][2] = l[0][2];
+		l[1][2] = l[0][1];
+		l[0][2] = temp00;
+		l[0][1] = temp10;
+		System.out.println("");
 	}
 	
-	void leftPrime() {
-		String temp00 = up[0][0];
-		String temp10 = up[1][0];
-		String temp20 = up[2][0];
-		up[0][0] = front[0][0];
-		up[1][0] = front[1][0];
-		up[2][0] = front[2][0];
-		front[0][0] = down[0][0];
-		front[1][0] = down[2][0];
-		front[2][0] = down[2][0];
-		down[0][0] = back[0][0];
-		down[1][0] = back[1][0];
-		down[2][0] = back[2][0];
-		back[0][0] = temp00;
-		back[1][0] = temp10;
-		back[2][0] = temp20;
-		temp00 = left[0][0];
-		temp10 = left[1][0];
-		left[0][0] = left[0][2];
-		left[1][0] = left[0][1];
-		left[0][2] = left[2][2];
-		left[0][1] = left[1][2];
-		left[2][2] = left[2][0];
-		left[1][2] = left[2][1];
-		left[2][0] = temp00;
-		left[2][1] = temp10;
+	void lP() {
+		String temp00 = u[0][0];
+		String temp10 = u[1][0];
+		String temp20 = u[2][0];
+		u[0][0] = f[0][0];
+		u[1][0] = f[1][0];
+		u[2][0] = f[2][0];
+		f[0][0] = d[0][0];
+		f[1][0] = d[2][0];
+		f[2][0] = d[2][0];
+		d[0][0] = b[0][0];
+		d[1][0] = b[1][0];
+		d[2][0] = b[2][0];
+		b[0][0] = temp00;
+		b[1][0] = temp10;
+		b[2][0] = temp20;
+		temp00 = l[0][0];
+		temp10 = l[1][0];
+		l[0][0] = l[0][2];
+		l[1][0] = l[0][1];
+		l[0][2] = l[2][2];
+		l[0][1] = l[1][2];
+		l[2][2] = l[2][0];
+		l[1][2] = l[2][1];
+		l[2][0] = temp00;
+		l[2][1] = temp10;
+		System.out.println("");
 	}
 	
+	void m() {
+		String temp00 = u[0][1];
+		String temp10 = u[1][1];
+		String temp20 = u[2][1];
+		u[0][1] = b[0][1];
+		u[1][1] = b[1][1];
+		u[2][1] = b[2][1];
+		b[0][1] = d[0][1];
+		b[1][1] = d[2][1];
+		b[2][1] = d[2][1];
+		d[0][1] = f[0][1];
+		d[1][1] = f[1][1];
+		d[2][1] = f[2][1];
+		f[0][1] = temp00;
+		f[1][1] = temp10;
+		f[2][1] = temp20;
+		temp00  = l[0][0];
+		temp10  = l[1][0];
+		l[0][0] = l[2][0];
+		l[1][0] = l[2][1];
+		l[2][0] = l[2][2];
+		l[2][1] = l[1][2];
+		l[2][2] = l[0][2];
+		l[1][2] = l[0][1];
+		l[0][2] = temp00;
+		l[0][1] = temp10;
+		System.out.println("");
+	}
+	
+	
+	void mP() {
+		String temp00 = u[0][1];
+		String temp10 = u[1][1];
+		String temp20 = u[2][1];
+		u[0][1] = f[0][1];
+		u[1][1] = f[1][1];
+		u[2][1] = f[2][1];
+		f[0][1] = d[0][1];
+		f[1][1] = d[2][1];
+		f[2][1] = d[2][1];
+		d[0][1] = b[0][1];
+		d[1][1] = b[1][1];
+		d[2][1] = b[2][1];
+		b[0][1] = temp00;
+		b[1][1] = temp10;
+		b[2][1] = temp20;
+		temp00  = l[0][0];
+		temp10  = l[1][0];
+		l[0][0] = l[2][0];
+		l[1][0] = l[2][1];
+		l[2][0] = l[2][2];
+		l[2][1] = l[1][2];
+		l[2][2] = l[0][2];
+		l[1][2] = l[0][1];
+		l[0][2] = temp00;
+		l[0][1] = temp10;
+		System.out.println("");
+	}
+	
+	void r() {
+		String temp00 = u[0][2];
+		String temp10 = u[1][2];
+		String temp20 = u[2][2];
+		u[0][2] = b[0][2];
+		u[1][2] = b[1][2];
+		u[2][2] = b[2][2];
+		b[0][2] = d[0][2];
+		b[1][2] = d[2][2];
+		b[2][2] = d[2][2];
+		d[0][2] = f[0][2];
+		d[1][2] = f[1][2];
+		d[2][2] = f[2][2];
+		f[0][2] = temp00;
+		f[1][2] = temp10;
+		f[2][2] = temp20;
+		temp00  = l[0][0];
+		temp10  = l[1][0];
+		l[0][0] = l[2][0];
+		l[1][0] = l[2][1];
+		l[2][0] = l[2][2];
+		l[2][1] = l[1][2];
+		l[2][2] = l[0][2];
+		l[1][2] = l[0][1];
+		l[0][2] = temp00;
+		l[0][1] = temp10;
+		System.out.println("");
+	}
+	
+	void rP() {
+		String temp00 = u[0][2];
+		String temp10 = u[1][2];
+		String temp20 = u[2][2];
+		u[0][2] = f[0][2];
+		u[1][2] = f[1][2];
+		u[2][2] = f[2][2];
+		f[0][2] = d[0][2];
+		f[1][2] = d[2][2];
+		f[2][2] = d[2][2];
+		d[0][2] = b[0][2];
+		d[1][2] = b[1][2];
+		d[2][2] = b[2][2];
+		b[0][2] = temp00;
+		b[1][2] = temp10;
+		b[2][2] = temp20;
+		temp00  = l[0][0];
+		temp10  = l[1][0];
+		l[0][0] = l[2][0];
+		l[1][0] = l[2][1];
+		l[2][0] = l[2][2];
+		l[2][1] = l[1][2];
+		l[2][2] = l[0][2];
+		l[1][2] = l[0][1];
+		l[0][2] = temp00;
+		l[0][1] = temp10;
+		System.out.println("");
+	}
+
 	// rotate whole clockwise
 	void y() {
-		String[][] temp = front;
-		front = right;
-		right = back;
-		back = left;
-		left = temp;
-		String temp00 = up[0][0];
-		String temp10 = up[1][0];
-		up[0][0] = up[2][0];
-		up[1][0] = up[2][1];
-		up[2][0] = up[2][2];
-		up[2][1] = up[1][2];
-		up[2][2] = up[0][2];
-		up[1][2] = up[0][1];
-		up[0][2] = temp00;
-		up[0][1] = temp10;
-		temp00 = down[0][0];
-		temp10 = down[1][0];
-		down[0][0] = down[0][2];
-		down[1][0] = down[0][1];
-		down[0][2] = down[2][2];
-		down[0][1] = down[1][2];
-		down[2][2] = down[2][0];
-		down[1][2] = down[2][1];
-		down[2][0] = temp00;
-		down[2][1] = temp10;
+		String[][] temp = f;
+		f = r;
+		r = b;
+		b = l;
+		l = temp;
+		String temp00 = u[0][0];
+		String temp10 = u[1][0];
+		u[0][0] = u[2][0];
+		u[1][0] = u[2][1];
+		u[2][0] = u[2][2];
+		u[2][1] = u[1][2];
+		u[2][2] = u[0][2];
+		u[1][2] = u[0][1];
+		u[0][2] = temp00;
+		u[0][1] = temp10;
+		temp00 = d[0][0];
+		temp10 = d[1][0];
+		d[0][0] = d[0][2];
+		d[1][0] = d[0][1];
+		d[0][2] = d[2][2];
+		d[0][1] = d[1][2];
+		d[2][2] = d[2][0];
+		d[1][2] = d[2][1];
+		d[2][0] = temp00;
+		d[2][1] = temp10;
+		System.out.println("");
 	}
 	
 	void print() {
-		for(String[] i : back) {
+		for(String[] i : b) {
 			System.out.println("");
 			for(int k = 0 ; k < 6; k++) {
 				System.out.print(" ");
@@ -122,17 +242,17 @@ public class Cube {
 		}
 		for(int i = 0; i < 3 ; i++) {
 			System.out.println("");
-			for(String j : left[i]) {
+			for(String j : l[i]) {
 				System.out.print(" "+j);
 			}
-			for(String j : up[i]) {
+			for(String j : u[i]) {
 				System.out.print(" "+j);
 			}
-			for(String j : right[i]) {
+			for(String j : r[i]) {
 				System.out.print(" "+j);
 			}
 		}
-		for(String[] i : front) {
+		for(String[] i : f) {
 			System.out.println("");
 			for(int k = 0 ; k < 6; k++) {
 				System.out.print(" ");
@@ -141,7 +261,7 @@ public class Cube {
 				System.out.print(" "+j);
 			}
 		}
-		for(String[] i : down) {
+		for(String[] i : d) {
 			System.out.println("");
 			for(int k = 0 ; k < 6; k++) {
 				System.out.print(" ");
--- rubiks-cube/src/Play.java
+++ rubiks-cube/src/Play.java
@@ -2,8 +2,8 @@ public class Play {
 	public static void main(String[]args){
 		Cube cube = new Cube();
 		cube.print();
-		cube.y();
-		System.out.println("");
+		cube.r();
+		cube.rP();
 		cube.print();
 	}
 }
